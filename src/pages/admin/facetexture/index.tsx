ad6771d9d78f52124229d7103b8ef96a11bbf14c
Guilherme Henrique <ghpm99@hotmail.com>
2022-06-26 15:01:42 -0300

    Feat: implements reorder and drag and drop

--- src/pages/admin/facetexture/index.tsx
+++ src/pages/admin/facetexture/index.tsx
@@ -1,5 +1,5 @@
 
-import { Breadcrumb, Checkbox, Image, Layout, message, Select, Upload } from 'antd';
+import { Breadcrumb, Button, Checkbox, Image, Layout, message, Select, Upload } from 'antd';
 import { PlusOutlined } from '@ant-design/icons';
 import { useEffect, useState } from 'react';
 import LoadingPage from '../../../components/loadingPage/Index';
@@ -9,7 +9,8 @@ import { fetchFaceTextureClassService, fetchFacetextureService, updateFacetextur
 import Styles from './Facetexture.module.css';
 import { db, Facetexture } from '../../../util/db';
 import { useLiveQuery } from 'dexie-react-hooks';
-
+import { RcFile } from 'antd/lib/upload';
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd'
 
 const { Header, Content } = Layout;
 
@@ -23,7 +24,7 @@ function FaceTexture() {
     const [loading, setLoading] = useState(true)
 
     const facetexture = useLiveQuery(
-        () => db.facetexture.toArray()
+        () => db.facetexture.orderBy('order').toArray()
     )
 
     useEffect(() => {
@@ -58,7 +59,7 @@ function FaceTexture() {
         const classObject = classBdo.find(
             item => item.id === facetexture.class
         )
-        const blob = await fetch(`/facetexture/${classObject.name ?? 'default'}.png`).then(r => r.blob())
+        const blob = await fetch(`/facetexture/${classObject?.name ?? 'default'}.png`).then(r => r.blob())
 
         await db.facetexture.add({
             ...facetexture,
@@ -93,9 +94,11 @@ function FaceTexture() {
 
         const blob = await fetch('/facetexture/default.png').then(r => r.blob())
 
+        const lastOrder = ((await db.facetexture.orderBy('order').last())?.order ?? 0) + 1
+
         const id = await db.facetexture.add({
             name: 'default.png',
-            order: 0,
+            order: lastOrder,
             show: true,
             image: blob,
         })
@@ -142,11 +145,76 @@ function FaceTexture() {
         setSelected(facetexture)
     }
 
-    const updateImageSelectedCharacter = (id, file) => {
-        console.log(id, file)
+    const updateImageSelectedCharacter = (id, file: RcFile) => {
+        setSelected(prev => ({
+            ...prev,
+            name: file.name,
+            image: file,
+        }))
+        db.facetexture.update(id, {
+            name: file.name,
+            image: file,
+        })
+        updateFacetexture()
     }
 
-    if(loading){
+    const deleteCharacter = (id) => {
+        db.facetexture.delete(id)
+        setSelected(undefined)
+        updateFacetexture()
+    }
+
+    const reorderCharacter = async (facetexture: Facetexture, newOrder: number) => {
+        if (newOrder <= 0) {
+            return
+        }
+        //source = id: 15 order:4
+        //destination = id:16 order:5
+        // 5 > 4
+        console.log('source', facetexture.order)
+        console.log('destination', newOrder)
+        if (newOrder > facetexture.order) {
+            const facetexturesMiddle = await db.facetexture.where('order').between(facetexture.order, newOrder + 1).toArray()
+            console.log('middle', facetexturesMiddle)
+            for (const facetextureMiddle of facetexturesMiddle) {
+                console.log(facetextureMiddle.order)
+                db.facetexture.update(facetextureMiddle.id, {
+                    order: facetextureMiddle.order - 1
+                })
+            }
+        }
+
+        const facetexturesAfter = await db.facetexture.where('order').aboveOrEqual(newOrder).toArray()
+        console.log('after', facetexturesAfter)
+
+        for (const facetextureAfter of facetexturesAfter) {
+            console.log(facetextureAfter.order)
+            db.facetexture.update(facetextureAfter.id, {
+                order: facetextureAfter.order + 1
+            })
+        }
+        console.log('destination', newOrder)
+        db.facetexture.update(facetexture.id, {
+            order: newOrder
+        })
+
+    }
+
+    const onDragEnd = async (result, provider) => {
+        console.log(result, provider)
+        if (!result.destination) {
+            return
+        }
+
+        const facetextureSource = await db.facetexture.where('id').equals(result.source.index).first()
+        const facetextureDestination = await db.facetexture.where('id').equals(result.destination.index).first()
+
+        reorderCharacter(facetextureSource, facetextureDestination.order)
+
+        updateFacetexture()
+    }
+
+    if (loading) {
         return <LoadingPage />
     }
 
@@ -165,32 +233,60 @@ function FaceTexture() {
                     <div className={ Styles['characters'] }>
                         <div>
                             <h1>Personagens</h1>
-                            <div className={ Styles['characters-container'] }>
-                                { facetexture?.map((character, index) => (
-                                    <div
-                                        key={ `character-${index}` }
-                                        className={ Styles['character'] }
-                                        onClick={ (event) => setSelectedCharacter(character.id) }
-                                    >
-                                        {
-                                            character.image &&
-                                            <img
-                                                src={ URL.createObjectURL(character.image) }
-                                                alt={ character.name }
-                                                width={ 125 }
-                                                height={ 160 }
-                                            />
-                                        }
-
-                                    </div>
-                                )) }
-                                <div
-                                    className={ Styles['include-characters'] }
-                                    onClick={ includeNewCharacteres }
+                            <DragDropContext onDragEnd={ onDragEnd }>
+                                <Droppable
+                                    droppableId='droppable'
+                                    type='CHARACTERS'
+                                    direction='horizontal'
                                 >
-                                    +
-                                </div>
+                                    { (provided, snapshot) => (
+                                        <div
+                                            { ...provided.droppableProps }
+                                            ref={ provided.innerRef }
+                                            className={ Styles['characters-container'] }
+                                        >
+                                            { facetexture?.map((character, index) => (
+                                                <Draggable
+                                                    key={ character.id.toString() }
+                                                    draggableId={ character.id.toString() }
+                                                    index={ character.id }
+
+                                                >
+                                                    { (provided, snapshot) => (
+                                                        <div
+                                                            ref={ provided.innerRef }
+                                                            { ...provided.dragHandleProps }
+                                                            { ...provided.draggableProps }
+                                                            key={ character.id.toString() }
+                                                            className={ Styles['character'] }
+                                                            onClick={ (event) => setSelectedCharacter(character.id) }
+                                                        >
+                                                            {
+                                                                character.image &&
+                                                                <img
+                                                                    src={ URL.createObjectURL(character.image) }
+                                                                    alt={ character.name }
+                                                                    width={ 125 }
+                                                                    height={ 160 }
+                                                                />
+                                                            }
+                                                        </div>
+                                                    ) }
+
+                                                </Draggable>
+                                            )) }
+                                            { provided.placeholder }
+                                        </div>
+                                    ) }
+                                </Droppable>
+                            </DragDropContext>
+                            <div
+                                className={ Styles['include-characters'] }
+                                onClick={ includeNewCharacteres }
+                            >
+                                +
                             </div>
+
                         </div>
                         <div className={ Styles['character-info'] }>
                             { selected &&
@@ -230,13 +326,48 @@ function FaceTexture() {
                                     <div>
                                         <Upload
                                             listType='picture-card'
-                                            beforeUpload={(file) => updateImageSelectedCharacter(selected.id, file)}
+                                            beforeUpload={ (file) => updateImageSelectedCharacter(selected.id, file) }
+                                            fileList={ [] }
                                         >
                                             <div>
                                                 <PlusOutlined />
                                                 <div style={ { marginTop: 8 } }>Upload</div>
                                             </div>
                                         </Upload>
+                                    </div>
+                                    <div>
+                                        <Button
+                                            type='primary'
+                                            onClick={ () => deleteCharacter(selected.id) }
+                                        >
+                                            Deletar personagem
+                                        </Button>
+                                    </div>
+                                    <div>
+                                        <Button
+                                            type='primary'
+                                            onClick={ () => {
+                                                reorderCharacter(selected, selected.order - 1)
+                                                setSelected(prev => ({
+                                                    ...prev,
+                                                    order: prev.order - 1
+                                                }))
+                                            } }
+                                        >
+                                            Mover para anterior
+                                        </Button>
+                                        <Button
+                                            type='primary'
+                                            onClick={ () => {
+                                                reorderCharacter(selected, selected.order + 1)
+                                                setSelected(prev => ({
+                                                    ...prev,
+                                                    order: prev.order + 1
+                                                }))
+                                            } }
+                                        >
+                                            Mover para proximo
+                                        </Button>
                                     </div>
                                 </>
                             }
--- src/util/db.ts
+++ src/util/db.ts
@@ -9,15 +9,22 @@ export interface Facetexture {
   image: Blob
 }
 
+export interface Background {
+  id?:number
+  image: Blob
+}
+
 export class MySubClassedDexie extends Dexie {
   // 'friends' is added by dexie when declaring the stores()
   // We just tell the typing system this is the case
   facetexture!: Table<Facetexture>;
+  background!: Table<Background>;
 
   constructor() {
     super('kawori');
     this.version(1).stores({
-      facetexture: '++id, name, class, show, order, image' // Primary key and indexed props
+      facetexture: '++id, name, class, show, order, image', // Primary key and indexed props
+      background: '++id, image'
     });
   }
 }
