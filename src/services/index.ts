beed8e066e8783675ee699ada2498cf07907949d
Guilherme <ghpm99@hotmail.com>
2022-11-12 00:52:53 -0300

    Fix: include error 500

--- src/services/index.ts
+++ src/services/index.ts
@@ -34,7 +34,7 @@ apiDjango.interceptors.response.use(
         const originalRequest = config
 
         if (
-            (!error.response || response.status === 408 || response.status === 504) &&
+            (!error.response || response.status === 408 || response.status === 504 || response.status === 500) &&
             tried <= retryMaxCount
         ) {
             tried++
