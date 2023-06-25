068bc0cad8d442926e9bfe1fc8ae799faa113df0
Guilherme H <ghpm99@gmail.com>
2023-06-25 15:50:50 -0300

    Feat: Implements background animation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,37 +4,41 @@ import styles from "./Home.module.scss";
 
 export default function Home() {
     return (
-        <div className={styles["container"]}>
-            <MenuHeader />
-            <div className={styles["body"]}>
-                <h1 className={styles["title"]}>
-                    Cadastre-se e estilize sua tela de seleção agora mesmo!
-                </h1>
-                <div className={styles["signup-container"]}>
-                    <div className={styles["text-container"]}>
-                        <h1 className={styles["signup-text"]}>
-                            O cadastro é gratuito, simples e rapido.
-                        </h1>
+        <>
+            <div className={styles["container"]}>
+                <MenuHeader />
+                <div className={styles["body"]}>
+                    <h1 className={styles["title"]}>
+                        Cadastre-se e estilize sua tela de seleção agora mesmo!
+                    </h1>
+                    <div className={styles["signup-container"]}>
+                        <div className={styles["text-container"]}>
+                            <h1 className={styles["signup-text"]}>
+                                O cadastro é gratuito, simples e rapido.
+                            </h1>
+                        </div>
+                        <div className={styles["signup-form"]}>
+                            <div className={styles["form-title"]}>Cadastro</div>
+                            <SingupForm />
+                        </div>
+                        <div className={styles["text-container"]}>
+                            <h1 className={styles["signup-text"]}>
+                                Você está a apenas um passo de um novo nivel de
+                                personalização do seu jogo!
+                            </h1>
+                        </div>
                     </div>
-                    <div className={styles["signup-form"]}>
-                        <div className={styles["form-title"]}>Cadastro</div>
-                        <SingupForm />
+                    <div className={styles["footer-text"]}>
+                        Sinta-se a vontade para entrar para
+                        <a target="_blank" href="https://discord.gg/fykNkXyn2r">
+                            &nbsp;nossa comunidade&nbsp;
+                        </a>
+                        caso tenha alguma duvida ou sugestão!
                     </div>
-                    <div className={styles["text-container"]}>
-                        <h1 className={styles["signup-text"]}>
-                            Você está a apenas um passo de um novo nivel de
-                            personalização do seu jogo!
-                        </h1>
-                    </div>
-                </div>
-                <div className={styles["footer-text"]}>
-                    Sinta-se a vontade para entrar para
-                    <a target="_blank" href="https://discord.gg/fykNkXyn2r">
-                        &nbsp;nossa comunidade&nbsp;
-                    </a>
-                    caso tenha alguma duvida ou sugestão!
+                    <div className={styles["body-background"]} />
                 </div>
             </div>
-        </div>
+            <div className={styles["background"]} />
+        </>
     );
 }
