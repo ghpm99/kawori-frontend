7d2ce42819a4453be82e06d0071fdc836c0961d0
Guilherme H <ghpm99@gmail.com>
2023-07-24 16:33:14 -0300

    Fix: Config lint

--- src/pages/admin/facetexture/index.tsx
+++ src/pages/admin/facetexture/index.tsx
@@ -1,27 +1,24 @@
-import { Breadcrumb, Layout, message } from "antd"
-import { useRouter } from "next/router"
-import { useEffect } from "react"
-import { useSelector } from "react-redux"
+import { Breadcrumb, Layout, message } from "antd";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
 
-import Background from "../../../components/facetexture/background"
-import Characters from "../../../components/facetexture/characters"
-import Loading from "../../../components/facetexture/loading"
-import Preview from "../../../components/facetexture/preview"
-import LoginHeader from "../../../components/loginHeader/Index"
-import MenuAdmin from "../../../components/menuAdmin/Index"
-import {
-    IFacetextureCharacterApi,
-    updateFacetextureService,
-} from "../../../services/facetexture"
+import Background from "../../../components/facetexture/background";
+import Characters from "../../../components/facetexture/characters";
+import Loading from "../../../components/facetexture/loading";
+import Preview from "../../../components/facetexture/preview";
+import LoginHeader from "../../../components/loginHeader/Index";
+import MenuAdmin from "../../../components/menuAdmin/Index";
+import { IFacetextureCharacterApi, updateFacetextureService } from "../../../services/facetexture";
 import {
     fetchFacetexture,
     setFacetextureIsEdited,
     updateBackgroundReducer,
     updateFacetextureUrlReducer,
-} from "../../../store/features/facetexture"
-import { RootState, useAppDispatch } from "../../../store/store"
-import { db } from "../../../util/db"
-import Styles from "./Facetexture.module.scss"
+} from "../../../store/features/facetexture";
+import { RootState, useAppDispatch } from "../../../store/store";
+import { db } from "../../../util/db";
+import Styles from "./Facetexture.module.scss";
 
 const { Header, Content } = Layout;
 
@@ -32,7 +29,7 @@ function FaceTexture() {
 
     const dispatch = useAppDispatch();
     const facetextureStore = useSelector(
-        (state: RootState) => state.facetexture
+        (state: RootState) => state.facetexture,
     );
 
     useEffect(() => {
@@ -107,7 +104,7 @@ function FaceTexture() {
                 updateFacetextureUrlReducer({
                     index: index,
                     image: imageUrl,
-                })
+                }),
             );
         }
     };
