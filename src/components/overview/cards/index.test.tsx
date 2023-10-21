d49074e86095f714554df534d35bfa29e50f86e2
Guilherme H <ghpm99@gmail.com>
2023-10-21 00:45:38 -0300

    Fix: update dependencies

--- src/components/overview/cards/index.test.tsx
+++ src/components/overview/cards/index.test.tsx
@@ -1,9 +1,17 @@
-import Cards from "@/components/overview/cards"
-import { render, screen } from "@testing-library/react"
+import Cards from "@/components/overview/cards";
+import { render, screen } from "@testing-library/react";
 
 describe("Overview Card", () => {
     it("Render overview card", () => {
-        render(<Cards loading={false} amountPayment={1000.00} amountPaymentClosed={1} amountPaymentOpen={1} countPayment={1} />);
-        expect("Total de pagamentos").toBeInTheDocument()
-    })
+        render(
+            <Cards
+                loading={false}
+                amountPayment={1000.0}
+                amountPaymentClosed={1}
+                amountPaymentOpen={1}
+                countPayment={1}
+            />,
+        );
+        expect("Total de pagamentos").toBeInTheDocument();
+    });
 });
--- src/components/overview/cards/snapshot.tsx
+++ src/components/overview/cards/snapshot.tsx
@@ -1,7 +1,9 @@
-import { render } from '@testing-library/react'
-import Cards from "@/components/overview/cards"
+import { render } from "@testing-library/react";
+import Cards from "@/components/overview/cards";
 
-it('renders homepage unchanged', () => {
-  const { container } = render(<Cards loading={false} amountPayment={1000.00} amountPaymentClosed={1} amountPaymentOpen={1} countPayment={1} />)
-  expect(container).toMatchSnapshot()
-})
+it("renders homepage unchanged", () => {
+    const { container } = render(
+        <Cards loading={false} amountPayment={1000.0} amountPaymentClosed={1} amountPaymentOpen={1} countPayment={1} />,
+    );
+    expect(container).toMatchSnapshot();
+});
