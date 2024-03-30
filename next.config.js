061fecf1dd62b936b842c7523faf2a5b8fd2550e
Guilherme H <ghpm99@gmail.com>
2024-03-30 11:58:35 -0300

    Fix: Remove favicons lib and include favicon.icon

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
 /** @type {import('next').NextConfig} */
-const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 const nextConfig = {
   reactStrictMode: false,
@@ -14,35 +12,7 @@ const nextConfig = {
     'rc-tooltip' ,
     'rc-tree',
     'rc-table',
-    '@tsparticles',
-    'tsparticles',
-    'tsparticles-engine'
-  ],
-  webpack: (config, { dev }) => {
-    config.plugins.push(
-        new FaviconsWebpackPlugin({
-            logo: './public/favicon.jpg',
-            cache: true,
-            inject: true,
-            favicons: {
-                appName: 'Kawori',
-                appDescription: 'Kawori website',
-                developerName: 'KaworiChan',
-                developerURL: null,
-                background: '#fff',
-                theme_color: '#333',
-                icons: {
-                    coast: false,
-                    yandex: false
-                }
-            }
-        })
-    )
-    config.plugins.push(
-        new HtmlWebpackPlugin()
-    )
-    return config
-  }
+  ]
 }
 
 module.exports = nextConfig
