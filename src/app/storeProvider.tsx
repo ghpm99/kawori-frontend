da044cf2c33bfc0d9722302bb7ff587b1498dba2
Guilherme H <ghpm99@gmail.com>
2024-10-22 00:22:21 -0300

    feat: include react-hooks deps

--- src/app/storeProvider.tsx
+++ src/app/storeProvider.tsx
@@ -1,6 +1,6 @@
-import { AppStore, store } from "@/lib/store"
-import { useRef } from "react"
-import { Provider } from "react-redux"
+import { AppStore, store } from "@/lib/store";
+import { useRef } from "react";
+import { Provider } from "react-redux";
 
 export default function StoreProvider({ children }: { children: React.ReactNode }) {
     const storeRef = useRef<AppStore>();
