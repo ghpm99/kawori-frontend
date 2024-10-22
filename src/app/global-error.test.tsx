4398fa9cd26297e7cec1ec5da3fd90cb0c305621
Guilherme H <ghpm99@gmail.com>
2024-10-22 00:20:30 -0300

    feat: global-error test

--- src/app/global-error.test.tsx
+++ src/app/global-error.test.tsx
@@ -1,13 +1,12 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen } from "@testing-library/react";
 import * as Sentry from "@sentry/nextjs";
 import GlobalError from "@/app/global-error";
-
 
 describe("GlobalError", () => {
     test("should render the error message", () => {
         render(<GlobalError error="error" reset={() => {}} />);
         expect(screen.getByText("Something went wrong!")).toBeInTheDocument();
         expect(screen.getByText("Try again")).toBeInTheDocument();
-        expect(Sentry.captureException).toHaveBeenCalledWith("error")
-    })
-})
+        expect(Sentry.captureException).toHaveBeenCalledWith("error");
+    });
+});
