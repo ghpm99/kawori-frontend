33f443fe39c6cebf2a4de7379709124aa851a3fe
Guilherme H <ghpm99@gmail.com>
2024-11-19 23:27:44 -0300

    feat(speedInsights): adiciona speedInsights no projeto

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { AntdRegistry } from "@ant-design/nextjs-registry";
 import "../../styles/globals.scss";
 import StoreProvider from "./storeProvider";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
@@ -14,6 +15,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                         <AuthProvider>
                             {children}
                             <Analytics />
+                            <SpeedInsights />
                         </AuthProvider>
                     </AntdRegistry>
                 </StoreProvider>
