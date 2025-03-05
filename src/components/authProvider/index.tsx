2a1ee016c6a6df291ae87474bfb0205d7bf730cd
Guilherme H. <ghpm99@gmail.com>
2025-03-05 09:45:50 -0300

    fix: remove validacao se existe token para verificar se e valido

--- src/components/authProvider/index.tsx
+++ src/components/authProvider/index.tsx
@@ -1,7 +1,6 @@
 import { userDetailThunk, userGroupsThunk, verifyTokenThunk } from "@/lib/features/auth";
 import { useAppDispatch, useAppSelector } from "@/lib/hooks";
 import { useEffect } from "react";
-import { LOCAL_STORE_ITEM_NAME } from "../constants";
 
 const AuthProvider = ({ children }) => {
     const dispatch = useAppDispatch();
@@ -13,22 +12,8 @@ const AuthProvider = ({ children }) => {
         dispatch(userGroupsThunk());
     };
 
-    const verifyLocalStore = (): boolean => {
-        const localStorageToken = localStorage.getItem(LOCAL_STORE_ITEM_NAME);
-
-        if (!localStorageToken) {
-            return false;
-        }
-        const dateNow = new Date();
-        const tokenDate = new Date(localStorageToken);
-
-        return dateNow < tokenDate;
-    };
-
     useEffect(() => {
-        if (verifyLocalStore()) {
-            dispatch(verifyTokenThunk());
-        }
+        dispatch(verifyTokenThunk());
     }, []);
 
     useEffect(() => {
